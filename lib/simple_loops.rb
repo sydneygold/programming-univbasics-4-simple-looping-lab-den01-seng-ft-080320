5553268e6b14076906983beff20e71312df62539
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-16 20:55:36 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,7 +25,8 @@ def output_array(array)
 def return_string_array(array)
   count = 0
   while count < array.length do
-    array[count].to_s
+    new_array.push(array[count].to_s)
     count += 1
   end
+  new_array
   end
