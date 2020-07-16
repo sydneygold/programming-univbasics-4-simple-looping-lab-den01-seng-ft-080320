dcbab7c5dba7794618f6c5cd24e0dd288210c73f
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-16 20:34:50 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,9 @@
-# Write your methods here
+def loop_message_five_times(array, parameter)
+  counter = 0
+
+  while counter < array >= 5 do
+    puts array[counter]
+    counter += 1
+  end
+    
+  end
